ee6defc412fba5750c1faff08c780bca31296480
project-ryu <bryu94@gmail.com>
2024-11-12 14:15:10 -0600

    Austin mentoring
    - refined animations
    - aspect ratio/scaling
    - introduce CardView
    - changed background

--- Concentration2.0/ViewController.swift
+++ Concentration2.0/ViewController.swift
@@ -7,53 +7,70 @@
 
 import UIKit
 
-let x = ViewController.GameState()
+class CardView: UIImageView {
+    var faceImage: UIImage?
+}
 
 class ViewController: UIViewController {
     struct GameState {
-        var cardViews: [UIImageView] = []
+        var cardViews: [CardView] = []
         var matchedPairs = 0
     }
     
-    var cardFaces: [UIImage] = [
-        UIImage(named: "clubs")!,
-        UIImage(named: "clubs")!,
-        UIImage(named: "hearts")!,
-        UIImage(named: "hearts")!,
-        UIImage(named: "spades")!,
-        UIImage(named: "spades")!,
-        UIImage(named: "diamonds")!,
-        UIImage(named: "diamonds")!
-    ]
-    
-    var selectedCards: [UIImageView] = []
+    var selectedCards: [CardView] = []
     
     var isRevealingCards = false
     
     private var gameState = GameState()
     
+    let background = UIImageView(image: UIImage(named: "Card-Mahjong")!)
+    //rename to puzzleCard 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let background = UIImageView(frame: UIScreen.main.bounds)
-        background.image = UIImage(named: "Card-Mahjong")
-        background.contentMode = .scaleAspectFill
+        
+        let aspect_ratio = background.bounds.height / background.bounds.width
+        
+        let width = view.bounds.width * 0.9
+        let height = aspect_ratio * width
+
+        background.bounds.size.width = width
+        background.bounds.size.height = height
+        
+        background.center.x = view.bounds.width / 2
+        background.center.y = view.bounds.height / 2
+
+       // view.insertSubview(background, at: 0)
         view.addSubview(background)
-        view.sendSubviewToBack(background)
+        // view.sendSubviewToBack(background)
         
         setupGame()
     }
     
     func setupGame() {
         gameState = GameState()
-
-        cardFaces.shuffle()
         
-        var y = CGFloat(310)
-        var x = CGFloat(100)
+        let cardFaces: [UIImage] = [
+            UIImage(named: "clubs")!,
+            UIImage(named: "clubs")!,
+            UIImage(named: "hearts")!,
+            UIImage(named: "hearts")!,
+            UIImage(named: "spades")!,
+            UIImage(named: "spades")!,
+            UIImage(named: "diamonds")!,
+            UIImage(named: "diamonds")!
+        ].shuffled()
         
-        for index in 1...cardFaces.count {
-            let subview = UIImageView(frame: CGRect(x: x, y: y, width: 70, height: 100))
+        for faceImage in cardFaces {
+            let image_width = view.bounds.width * 0.15
+            let image_height = image_width * 1.43
+            let subview = CardView(frame: CGRect(x: 0, y: 0, width: image_width, height: image_height))
+            subview.faceImage = faceImage
+            subview.center.x = view.bounds.width * 0.5
+            subview.center.y = view.bounds.width * -0.15
+            // same relative distance for any device
+            // change point values to percentages for relative scaling, example: 70/width of iphone17 * width of view.bounds
             subview.image = UIImage(named: "suits")
             subview.isUserInteractionEnabled = true
             
@@ -62,10 +79,45 @@ class ViewController: UIViewController {
             subview.layer.shadowRadius = 3
             subview.layer.shadowColor = UIColor.black.cgColor
             subview.layer.shadowOffset = CGSize(width: 0, height: 1)
-
+            
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tappedCard(_:)))
             subview.addGestureRecognizer(tapGesture)
             
+            
+            // eventually change out code to be mathematically centered, or construct the grid from scratch
+            view.addSubview(subview)
+            gameState.cardViews.append(subview)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            UIView.animate(
+                withDuration: 0.5,
+                animations: {
+                    self.layoutCardGrid()
+                }
+            )
+        }
+    }
+    func shuffleCards () {
+        gameState.cardViews = gameState.cardViews.shuffled()
+        layoutCardGrid()
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        UIView.animate(
+            withDuration: 0.5,
+            animations: {
+                self.shuffleCards()
+            }
+        )
+    }
+    
+    func layoutCardGrid () {
+        var y = CGFloat(310)
+        var x = CGFloat(100)
+        
+        for index in 1...gameState.cardViews.count {
+            let cardView = gameState.cardViews[index - 1]
+            cardView.frame.origin.x = x
+            cardView.frame.origin.y = y
             if index % 2 == 0 {
                 x = 100
                 y += 110
@@ -73,35 +125,29 @@ class ViewController: UIViewController {
             else {
                 x += 120
             }
-            
-            view.addSubview(subview)
-            gameState.cardViews.append(subview)
         }
     }
-    
     @objc func tappedCard(_ sender: UITapGestureRecognizer) {
         guard !isRevealingCards else { return }
-        guard let tappedCard = sender.view as? UIImageView else { return }
+        guard let tappedCard = sender.view as? CardView else { return }
 
-        if let index = gameState.cardViews.firstIndex(of: tappedCard) {
-            UIView.transition(
-                with: tappedCard,
-                duration: 0.3,
-                options: .transitionFlipFromLeft,
-                animations: {
-                    tappedCard.image = self.cardFaces[index]
-                },
-                completion: nil
-            )
-            
-            selectedCards.append(tappedCard)
-            
-            if selectedCards.count == 2 {
-                isRevealingCards = true
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                    self.matchCheck()
-                    self.isRevealingCards = false
-                }
+        UIView.transition(
+            with: tappedCard,
+            duration: 0.3,
+            options: .transitionFlipFromLeft,
+            animations: {
+                tappedCard.image = tappedCard.faceImage
+            },
+            completion: nil
+        )
+        
+        selectedCards.append(tappedCard)
+        
+        if selectedCards.count == 2 {
+            isRevealingCards = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                self.matchCheck()
+                self.isRevealingCards = false
             }
         }
     }
@@ -157,6 +203,8 @@ class ViewController: UIViewController {
     }
 }
 
-
+// change code for card views and positioning to be true center, or at least mathematically centered
 // add animation of cards being distributed
+    // func layoutCards --> go thru cards in order and JUST lays out a grid
+    //
 // next add animate shuffling cards during setupGame
